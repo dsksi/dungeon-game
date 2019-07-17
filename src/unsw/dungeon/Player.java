9ebb81a5d4ca812d360f29310401fa3142c09435
Siyin Zhou <siyinz0208@gmail.com>
2019-07-17 16:44:40 +1000

    Update wall and player functions. Update player with sword pickup property

--- src/unsw/dungeon/Player.java
+++ src/unsw/dungeon/Player.java
@@ -2,6 +2,9 @@ package unsw.dungeon;
 
 import java.util.ArrayList;
 
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+
 /**
  * The player entity
  * @author Robert Clifton-Everest
@@ -10,6 +13,7 @@ import java.util.ArrayList;
 public class Player extends Entity {
 
     private Dungeon dungeon;
+    private IntegerProperty sword;
 
     /**
      * Create a player positioned in square (x,y)
@@ -19,6 +23,7 @@ public class Player extends Entity {
     public Player(Dungeon dungeon, int x, int y) {
         super(x, y);
         this.dungeon = dungeon;
+        this.sword = new SimpleIntegerProperty(0);
     }
 
     private boolean checkMoveable(int x, int y) {
@@ -28,34 +33,44 @@ public class Player extends Entity {
     	ArrayList<Entity> list = dungeon.getEntity(x, y);
         if(!list.isEmpty()) {
         	for (Entity e: list) {
-        		if(! e.movable()) return false;
+        		if(! e.movable(this)) return false;
             }
         }
         return true;
     }
     
     public void moveUp() {
-    	if(this.checkMoveable(this.getX(), this.getY() - 1))
+    	if(this.checkMoveable(this.getX(), this.getY() - 1)) {
+    		dungeon.interactItems(this, this.getX(), this.getY() - 1);
     		y().set(getY() - 1);
+    	}	
     }
 
     public void moveDown() {
-    	if(this.checkMoveable(this.getX(), this.getY() + 1))
+    	System.out.println("move down");
+    	if(this.checkMoveable(this.getX(), this.getY() + 1)) {
+    		dungeon.interactItems(this, this.getX(), this.getY() + 1);
     		y().set(getY() + 1);
+    	}
     }
 
     public void moveLeft() {
-    	if(this.checkMoveable(this.getX() - 1, this.getY()))
+    	if(this.checkMoveable(this.getX() - 1, this.getY())) {
+    		dungeon.interactItems(this, this.getX() - 1, this.getY());
     		x().set(getX() - 1);
+    	}
     }
 
     public void moveRight() {
-    	if(this.checkMoveable(this.getX() + 1, this.getY()))
+    	if(this.checkMoveable(this.getX() + 1, this.getY())) {
+    		dungeon.interactItems(this, this.getX()+1, this.getY());
     		x().set(getX() + 1);
+    		this.pickUpSword();
+    	}
     }
 
 	@Override
-	public boolean movable() {
+	public boolean movable(Entity obj) {
 		return true;
 	}
 
@@ -63,5 +78,13 @@ public class Player extends Entity {
 	public void interact(Entity obj) {
 		//TODO: implement enemy interaction with player
 		return;
+	}
+	
+	public IntegerProperty sword() {
+		return this.sword;
+	}
+	
+	public void pickUpSword() {
+		sword().set(1);
 	}
 }
