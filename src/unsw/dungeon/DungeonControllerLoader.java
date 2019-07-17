cf6578bad667cb55b920dc243fe73e47dcc15c94
Siyin Zhou <siyinz0208@gmail.com>
2019-07-17 16:40:33 +1000

    Add image of multiple entities and onload function for image view. Add track player sword status

--- src/unsw/dungeon/DungeonControllerLoader.java
+++ src/unsw/dungeon/DungeonControllerLoader.java
@@ -23,20 +23,36 @@ public class DungeonControllerLoader extends DungeonLoader {
 
     //Images
     private Image playerImage;
+    private Image playerSwordImage;
     private Image wallImage;
-
+    private Image enemyImage;
+    private Image exitImage;
+    private Image switchImage;
+    private Image treasureImage;
+    private Image boulderImage;
+    private Image swordImage;
+    private Image invImage;
+    
     public DungeonControllerLoader(String filename)
             throws FileNotFoundException {
         super(filename);
         entities = new ArrayList<>();
-        playerImage = new Image("/human_new.png");
+        playerImage = new Image("/player.png");
         wallImage = new Image("/brick_brown_0.png");
+        playerSwordImage = new Image("/playerSword.png");
+        enemyImage = new Image("/hound.png");
+        exitImage = new Image("/exit.png");
+        switchImage = new Image("/pressure_plate.png");
+        treasureImage = new Image("/gold_pile.png");
+        boulderImage = new Image("/boulder.png");
+        swordImage = new Image("/greatsword_1_new.png");
+        invImage = new Image("brilliant_blue_new.png");
     }
 
     @Override
     public void onLoad(Entity player) {
         ImageView view = new ImageView(playerImage);
-        addEntity(player, view);
+        addPlayer(player, view);
     }
 
     @Override
@@ -44,7 +60,54 @@ public class DungeonControllerLoader extends DungeonLoader {
         ImageView view = new ImageView(wallImage);
         addEntity(wall, view);
     }
+    
+    @Override
+    public void onLoad(Exit exit) {
+        ImageView view = new ImageView(exitImage);
+        addEntity(exit, view);
+    }
+    
+    @Override
+    public void onLoad(Enemy enemy) {
+    	ImageView view = new ImageView(enemyImage);
+    	addEntity(enemy, view);
+    }
+    
+    @Override
+    public void onLoad(Switch sw) {
+    	ImageView view = new ImageView(switchImage);
+    	addEntity(sw, view);
+    }
+    
+    @Override
+    public void onLoad(Treasure treasure) {
+    	ImageView view = new ImageView(treasureImage);
+    	addEntity(treasure, view);
+    }
+    
+    @Override
+    public void onLoad(Boulder boulder) {
+    	ImageView view = new ImageView(boulderImage);
+    	addEntity(boulder, view);
+    }
+    
+    @Override
+    public void onLoad(Sword sword) {
+    	ImageView view = new ImageView(swordImage);
+    	addEntity(sword, view);
+    }
 
+    @Override
+    public void onLoad(InvinciblePotion inv) {
+    	ImageView view = new ImageView(invImage);
+    	addEntity(inv, view);
+    }
+
+    private void addPlayer(Entity player, ImageView view) {
+    	trackPlayer(player, view);
+    	trackPosition(player, view);
+    	entities.add(view);
+    }
     private void addEntity(Entity entity, ImageView view) {
         trackPosition(entity, view);
         entities.add(view);
@@ -75,6 +138,31 @@ public class DungeonControllerLoader extends DungeonLoader {
             public void changed(ObservableValue<? extends Number> observable,
                     Number oldValue, Number newValue) {
                 GridPane.setRowIndex(node, newValue.intValue());
+            }
+        });
+        entity.status().addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> observable,
+                    Number oldValue, Number newValue) {
+            	if(!newValue.equals(oldValue))
+            		node.setVisible(false);
+            }
+        });
+    }
+    
+    private void trackPlayer(Entity entity, Node node) {
+    	Player player = (Player) entity;
+        GridPane.setColumnIndex(node, entity.getX());
+        GridPane.setRowIndex(node, entity.getY());
+        
+        player.sword().addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> observable,
+                    Number oldValue, Number newValue) {
+            	if(!newValue.equals(oldValue)) {
+            		ImageView view = (ImageView) node;
+            		view.setImage(playerSwordImage);
+            	}
             }
         });
     }
